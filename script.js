57f58b530c9288218c16c9925bf9f5370fbc812c
AhaSiwakorn <jshfhdhsxh@gmail.com>
2023-09-10 10:05:10 +0700

    first

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function showmodal(){
+document.getElementById("mymodal").style.display ="flex"
+}function off(){
+document.getElementById("mymodal").style.display ="none"}
+
+
+function showmodal1(){
+document.getElementById("mymodal1").style.display ="flex"
+}function off1(){
+document.getElementById("mymodal1").style.display ="none"}
+
+
+function showmodal2(){
+document.getElementById("mymodal2").style.display ="flex"
+}function off2(){
+document.getElementById("mymodal2").style.display ="none"}
+
+
+function showmodal3(){
+document.getElementById("mymodal3").style.display ="flex"
+}function off3(){
+document.getElementById("mymodal3").style.display ="none"}
+
